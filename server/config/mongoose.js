e6e59265845d64a2ad6175583b9c4fd8562a65a5
DESKTOP-DJHI29G\FLY-HAWK <mr.farooq.ok@gmail.com>
2020-06-19 18:46:26 +0500

    -env changes

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -7,6 +7,7 @@ const config = require('./config');
 // connect to mongo db
 const mongoUri = config.mongo.host;
 mongoose.connect(mongoUri, { keepAlive: 1 });
+console.log('DB Connected :: 😃')
 mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${mongoUri}`);
 });
