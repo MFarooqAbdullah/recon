b37c0b947818c9655ead53cd5228b9a0411f8010
DESKTOP-DJHI29G\FLY-HAWK <mr.farooq.ok@gmail.com>
2020-06-20 12:13:47 +0500

    -update

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -6,14 +6,14 @@ const config = require('./config');
 
 // connect to mongo db
 const mongoUri = config.mongo.host;
-mongoose.connect(mongoUri, { keepAlive: 1 });
+mongoose.connect(mongoUri, { keepAlive: 1, useNewUrlParser: true, useUnifiedTopology: true });
 console.log('DB Connected :: 😃')
 mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${mongoUri}`);
 });
 
 // print mongoose logs in dev env
-if (config.MONGOOSE_DEBUG) {
+if (config.MONGOOSE_DEBUG) {  
   mongoose.set('debug', (collectionName, method, query, doc) => {
     debug(`${collectionName}.${method}`, util.inspect(query, false, 20), doc);
   });
